2a45dd8daf0bd32da79fe3a1f7f44a96dae48506
Marcos Volpi <marcos@marcosvolpi.com>
2022-04-09 16:40:09 -0300

    -Add front-end connection to ganache -Fix some issue with web3 and react/babel/webpack

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState, useCallback} from 'react'
+import Califications from './abis/Califications.json'
+
+var Web3 = require('web3');
+var contract;
+var currentAccount;
+const contractAddress = '0x9b1f7F645351AF3631a656421eD2e40f2802E6c0';
+
 
 function App() {
+  const [califications, setCalifications] = useState([]);
+
+
+  const fetchCalifications = useCallback(async () => {    
+    window.web3 = await new Web3('ws://127.0.0.1:8545');
+    await window.ethereum.enable();
+
+    const acc = window.web3.eth.getAccounts(console.log);
+    currentAccount = await window.ethereum.selectedAddress;
+    console.log('currentAccount', currentAccount);
+
+    contract = new window.web3.eth.Contract(Califications.abi, contractAddress);
+
+    //getCalifications();
+  }, [])
+
+  useEffect(() => {
+    fetchCalifications()
+  }, [fetchCalifications]) 
+  
+  
+
   return (
     <div className="App">
       <header className="App-header">
